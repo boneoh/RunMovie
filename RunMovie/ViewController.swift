3e347120600051dbc43b965efc5379fddb0b889b
Boneoh <boneoh@yahoo.com>
2022-12-11 13:25:48 -0700

    Changes to remove attempt at UDP
    Changes to remove attempt at UDP

--- RunMovie/ViewController.swift
+++ RunMovie/ViewController.swift
@@ -10,16 +10,9 @@ import AVKit
 import AVFoundation
 import Carbon.HIToolbox
 
-import Network
-
 class ViewController: NSViewController {
     @IBOutlet weak var playerView: AVPlayerView!
-    
-    var connection: NWConnection?
-    
-    var host: NWEndpoint.Host = "127.0.0.1"
-    var port: NWEndpoint.Port = 1000
-    
+        
     // var player: AVPlayer!
     
     override func viewDidLoad() {
@@ -126,92 +119,11 @@ class ViewController: NSViewController {
             
       
         //  player.play()
-        
-        
-        logger.log("*** In ViewController viewDidLoad - initialize UDP networking - connect")
-        
-        connect()
+
 
         logger.log("*** In ViewController viewDidLoad - viewDidLoad completed")
     }
     
-    
-      func send(_ payload: Data) {
-          connection!.send(content: payload, completion: .contentProcessed({ sendError in
-              if let error = sendError {
-                  NSLog("Unable to process and send the data: \(error)")
-              } else {
-                  NSLog("Data has been sent")
-                  self.connection!.receiveMessage { (data, context, isComplete, error) in
-                      guard let myData = data else { return }
-                      NSLog("Received message: " + String(decoding: myData, as: UTF8.self))
-                  }
-              }
-          }))
-      }
-      
-      func connect() {
-          connection = NWConnection(host: host, port: port, using: .udp)
-          
-          connection!.stateUpdateHandler = { (newState) in
-              switch (newState) {
-              case .preparing:
-                  NSLog("Entered state: preparing")
-              
-              case .ready:
-                      
-                  NSLog("Entered state: ready")
-                      
-                      self.connection!.receiveMessage { data, context, isComplete, error in
-                          if let unwrappedError = error {
-                              print("Error: NWError received in \(#function) - \(unwrappedError)")
-                              return
-                          }
-                          guard isComplete, let data = data else {
-                              print("Error: Received nil Data with context - \(String(describing: context))")
-                              return
-                          }
-                       
-                          var myData = data
-                          let keyCode = Int( myData.remove(at: 0) )
-                          
-                          let result = self.handleKeyPress(keyCode: keyCode )
-                      }
-                      
-              case .setup:
-                  NSLog("Entered state: setup")
-              case .cancelled:
-                  NSLog("Entered state: cancelled")
-              case .waiting:
-                  NSLog("Entered state: waiting")
-              case .failed:
-                  NSLog("Entered state: failed")
-              default:
-                  NSLog("Entered an unknown state")
-              }
-          }
-          
-          connection!.viabilityUpdateHandler = { (isViable) in
-              if (isViable) {
-                  NSLog("Connection is viable")
-              } else {
-                  NSLog("Connection is not viable")
-              }
-          }
-          
-          connection!.betterPathUpdateHandler = { (betterPathAvailable) in
-              if (betterPathAvailable) {
-                  NSLog("A better path is availble")
-              } else {
-                  NSLog("No better path is available")
-              }
-          }
-          
-          connection!.start(queue: .global())
-          
-          
-      }
-
     func handleKeyPress( keyCode: Int ) -> Bool
     {
         switch Int( keyCode) {
@@ -286,8 +198,8 @@ class ViewController: NSViewController {
         
             // handle keyDown only if current window has focus, i.e. is keyWindow
         
-          guard let locWindow = self.view.window,
-             NSApplication.shared.keyWindow === locWindow else { return false }
+        guard let locWindow = self.view.window,
+              NSApplication.shared.keyWindow === locWindow else { return false }
         
         let keyCode = Int( event.keyCode )
         
@@ -295,94 +207,7 @@ class ViewController: NSViewController {
         
         return result
         
-/*
-          switch Int( event.keyCode) {
-          
-          case kVK_Escape:          // Toggle Full Screen
-            
-              logger.log("*** In ViewController myKeyDown for <Esc>  - setting toggleFullScreen(true)")
-              
-              NSApplication.shared.mainWindow?.toggleFullScreen(true)
-                        
-             return true
-              
-          /*
-           case kVK_ANSI_H:             // Home = main display
-                         
-              logger.log("*** In ViewController myKeyDown for 'H'")
-              
-              processingHomeCommand = true
-              
-              if  isWindowFullScreen == true
-              {
-                  logger.log("*** In ViewController myKeyDown for 'H' - isWindowFullScreen == true")
-                  logger.log("*** In ViewController myKeyDown for 'H' - setting toggleFullScreen(true)")
-                  
-                  NSApplication.shared.mainWindow?.toggleFullScreen(true)
-              }
-              
-              let dx = 1920.0 / 2.0
-              let dy = 1080.0 / 2.0
-              
-              let screens = NSScreen.screens
-              var pos = NSPoint()
-              pos.x = screens[0].visibleFrame.midX - dx
-              pos.y = screens[0].visibleFrame.midY - dy
-              
-              DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                  NSApplication.shared.mainWindow?.setFrameOrigin(pos)
-                  NSApplication.shared.mainWindow?.toggleFullScreen(true)
-              }
-              
-              processingHomeCommand = false
-              
-              return true;
-        */
-
-          case kVK_ANSI_P:          // Play player
-              
-             player.play()
-              
-             return true
-              
-          case kVK_ANSI_Q:          // Quit player
-              
-             NSApplication.shared.terminate(self)
-              
-             return true
-              
-          case kVK_ANSI_R:          // Rewind player
-             
-             player.seek(to: .zero)
-              
-             return true
-              
-          case kVK_ANSI_S, kVK_Space: // Stop player
- 
-             player.pause()
-              
-             return true
-              
-          case kVK_LeftArrow:       // backward one frame
-              
-             player.pause()
-             playerItem?.step(byCount: -1)
-              
-             return true
-              
-          case kVK_RightArrow:      // forward one frame
-              
-             player.pause()
-             playerItem?.step(byCount: 1)
-              
-             return true
-
-          default:
-             return false
-          }
-*/
-        
-       }
+    }
     
     override var representedObject: Any? {
         didSet {
